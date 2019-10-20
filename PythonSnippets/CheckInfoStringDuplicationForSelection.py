22e291a263c63223b014a8447e95bfd6e7dc9bc7
MikiEremiki <mr.eremin.ma@ya.ru>
2019-10-21 01:16:49 +0300

    Create CheckInfoStringDuplicationForSelection.py

---
+++ PythonSnippets/CheckInfoStringDuplicationForSelection.py
@@ -0,0 +1,14 @@
+import re  # Не ясно зачем импорт, так как никаких функций из модуля в скрипте нет. Скрпит работает и без этой строки
+
+elemInfos = {}  # Объявили словарь
+
+print('{:-^40}'.format('CHECKING FOR DUPLICATE IDs'))  # Выводим в консоль строку с 40 символами
+for elemGuid in GetSelectedElements():  # Перебираем каждый виделенный элемент
+	elemInfos.setdefault(GetInfoString(elemGuid), set()).add(elemGuid)  # Для каждого элемента записываем в словарь
+# в качестве ключа ID, а в качестве занчений множество (set) из GUID
+
+print('Duplicated IDs:')
+for key, values in elemInfos.items():  # перебираем каждую пару ключ-значение
+	if len(values) > 1:  # Если длина значений, или кол-во GUID больше чем 1
+		print(key)  # значит печатаем ключ (одинаковые ID выбранных элементов элементов)
+print('{:-^40}'.format(''))
