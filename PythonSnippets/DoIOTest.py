c39c4b4ab5e33072e99660a6a4047f01beac9568
MikiEremiki <mr.eremin.ma@ya.ru>
2019-10-25 00:35:11 +0300

    Create DoIOTest.py

---
+++ PythonSnippets/DoIOTest.py
@@ -0,0 +1,4 @@
+Alert("Alert Dialog")  # Вызывает окно диалога
+print("Console Normal Output")  # Обычный принт
+PrintWarning("Console Warning Output")  # Принт с текстом желтого цвета
+PrintError("Console Error Output")  # Принт с текстом красного цвета
